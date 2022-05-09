7094fe5a48b6d8fa8d7107a7ff58f52cd9012a7e
azza14 <42944667+azza14@users.noreply.github.com>
2022-05-09 13:39:57 +0200

    Commit Implementation of Services & Repository GetCourses

---
+++ CleanArch/CleanArch.Infra.Data/Repository/CourseRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CleanArch.Domain;
+using CleanArch.Domain.Interfaces;
+using CleanArch.Infra.Data.Context;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDBContext _context;
+        public CourseRepository(UniversityDBContext context)
+        {
+            _context = context; 
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _context.Courses;
+        }
+    }
+}
