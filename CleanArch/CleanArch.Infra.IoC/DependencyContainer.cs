884b18602407e5f4b7f997c14424198d5986e9c5
azza14 <42944667+azza14@users.noreply.github.com>
2022-05-10 14:22:50 +0200

    Commit change of Presentation Layer

---
+++ CleanArch/CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Infra.Data.Repository;
+
+namespace CleanArch.Infra.IoC
+{
+   public class DependencyContainer
+    {
+        public static void RegisterService(IServiceCollection service)
+        {
+            //Application Layer
+            service.AddScoped<ICourseService,CourseService>();
+
+            // Infra.Data Layer
+            service.AddScoped<Domain.Interfaces.ICourseRepository, CourseRepository>();
+
+
+        }
+    }
+}
---
+++ CleanArch/CleanArch.Mvc/Controllers/CoursesController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CoursesController : Controller
+    {
+        private ICourseService _service;
+        public CoursesController(ICourseService service)
+        {
+            _service = service;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _service.GetCourses();
+            return View(model);
+        }
+    }
+}
